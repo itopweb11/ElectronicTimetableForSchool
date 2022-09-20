c31aa972977b72de523f5b4fa32af31089a6b9b7
itop55 <bachida.imihi@gmail.com>
2022-09-20 13:40:51 +0300

    Смена заголовка

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Электронное расписание для школы
         </a>
       </header>
     </div>
