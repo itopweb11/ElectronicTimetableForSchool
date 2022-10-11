d1bd3ce12cd96b8a696a45785420d3ca4221052b
itop55 <bachida.imihi@gmail.com>
2022-10-11 09:30:52 +0300

    Смена заголовка

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Электронное расписание для школы
+          Электронное расписание для школы.
         </a>
       </header>
     </div>
